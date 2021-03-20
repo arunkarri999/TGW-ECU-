fbeefdf41dbb2c0a2c00e84f1296ba4263ac8964
Arun Karri <arun.swed@outlook.com>
2021-03-21 00:18:14 +0100

    AVR EEPROM like implementation for Raspberry Pi

--- PiROM/ConsoleApplication1/ConsoleApplication1.cpp
+++ PiROM/ConsoleApplication1/ConsoleApplication1.cpp
@@ -146,6 +146,120 @@ extern "C" {
 }
 #endif
 
+/*
+ * PiEEPROM.h - AVR EEPROM like implementation for Raspberry Pi
+
+* /
+
+#ifndef __PiEEPROM_H__
+#define __PiEEPROM_H__ 1
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define EEPROM_SIZE 1024    // 1KB of EEPROM memory
+
+    extern uint8_t _eeprom[EEPROM_SIZE];
+
+    int eeprom_is_ready();
+    /**
+     * Loops until the eeprom is no longer busy.
+     * Returns Nothing.
+     */
+#define eeprom_busy_wait() do {} while (!eeprom_is_ready())
+
+
+     /**
+      * Read one byte from EEPROM address __p.
+      */
+    uint8_t eeprom_read_byte(const uint8_t* __p);
+
+    /**
+     * Read one 16-bit word (little endian) from EEPROM address __p.
+     */
+    uint16_t eeprom_read_word(const uint16_t* __p);
+
+    /**
+     * Read one 32-bit double word (little endian) from EEPROM address __p.
+     */
+    uint32_t eeprom_read_dword(const uint32_t* __p);
+
+    /**
+     * Read one float value (little endian) from EEPROM address __p.
+     */
+    float eeprom_read_float(const float* __p);
+
+    /**
+     * Read a block of __n bytes from EEPROM address __src to SRAM __dst.
+     */
+    void eeprom_read_block(void* __dst, const void* __src, size_t __n);
+
+
+
+    /**
+     * Write a byte __value to EEPROM address __p.
+     */
+    void eeprom_write_byte(uint8_t* __p, uint8_t __value);
+
+    /**
+     * Write a word __value to EEPROM address __p.
+     */
+    void eeprom_write_word(uint16_t* __p, uint16_t __value);
+
+    /**
+     * Write a 32-bit double word __value to EEPROM address __p.
+     */
+    void eeprom_write_dword(uint32_t* __p, uint32_t __value);
+
+    /**
+     * Write a float __value to EEPROM address __p.
+     */
+    void eeprom_write_float(float* __p, float __value);
+
+    /**
+     * Write a block of __n bytes to EEPROM address __dst from __src.
+     * The argument order is mismatch with common functions like strcpy().
+     */
+    void eeprom_write_block(const void* __src, void* __dst, size_t __n);
+
+
+
+    /**
+     * Update a byte __value to EEPROM address __p.
+     */
+#define eeprom_update_byte eeprom_write_byte
+
+     /**
+      * Update a word __value to EEPROM address __p.
+      */
+#define eeprom_update_word eeprom_write_word
+
+      /**
+       * Update a 32-bit double word __value to EEPROM address __p.
+       */
+#define eeprom_update_dword eeprom_write_dword
+
+       /**
+        * Update a float __value to EEPROM address __p.
+        */
+#define eeprom_update_float eeprom_write_float
+
+        /**
+         * Update a block of __n bytes to EEPROM address __dst from __src.
+         * The argument order is mismatch with common functions like strcpy().
+         */
+#define eeprom_update_block eeprom_write_block
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PiEEPROM_H__ */
+
 
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
